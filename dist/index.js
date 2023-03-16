fc9546a61a32cced525fe9f3e915fe8387a43648
Lex <58612086+LexAquifolium@users.noreply.github.com>
2023-03-16 12:46:47 +0100

    Depracted Body-parser ausgebaut

--- dist/index.js
+++ dist/index.js
@@ -6,7 +6,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
 const request_1 = __importDefault(require("request"));
-const body_parser_1 = __importDefault(require("body-parser"));
 const app = (0, express_1.default)();
 const userName = 'CSCHMAEING';
 const password = 'Taunusstein';
@@ -14,11 +13,12 @@ const auth = (userName + ":" + password);
 let csrfToken = "";
 let cookies = [];
 app.use((0, cors_1.default)());
-app.use(body_parser_1.default);
+app.use(express_1.default.urlencoded({ extended: true }));
+app.use(express_1.default.json()); // To parse the incoming requests with JSON payloads
 const port = 4000;
 app.get('/test', (req, res) => {
     console.log('Hello World');
-    res.send({ title: 'Ich werde zurückgegeben' });
+    // res.send({ title: 'Ich werde zurückgegeben' })
 });
 app.get('/authUser', (req, res) => {
     console.log("Bitte mach Dinge :(");
--- index.ts
+++ index.ts
@@ -1,30 +1,29 @@
 import express from 'express';
 import cors from 'cors';
 import request from 'request';
-import bodyParser from 'body-parser';
 
 const app = express();
 
-const userName = 'CSCHMAEING';
-const password = 'Taunusstein';
+const userName = 'NOPE';
+const password = 'NEINNEIN';
 const auth = (userName + ":" + password);
 let csrfToken: string = "";
 let cookies: Array<string> = [];
 
 app.use(cors());
-app.use(bodyParser);
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json()) // To parse the incoming requests with JSON payloads
+
 const port = 4000;
 
 
 app.get('/test', (req, res) => {
-
     console.log('Hello World');
     res.send({ title: 'Ich werde zurückgegeben' })
 })
 
 app.get('/authUser', (req, res) => {
-    console.log("Bitte mach Dinge :(");
-    res.send({ test: 'Hülfe' });
     const options = {
         url: 'http://ibssapdos.sap.ibs-banking.com:8000/sap/opu/odata/IBS/DIN_SCHULUNG_SRV/StandardSchulungenSet$format=json',
         headers: {
